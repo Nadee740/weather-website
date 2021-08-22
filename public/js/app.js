3d57d27c11ce77e9311ad135797e0125b349bb8f
Nadeem740 <nadeemblayparambil@gmail.com>
2021-08-22 10:02:52 +0530

    heorku setup

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ console.log("hy")
 weatherform.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location=search.value
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.Error)
         msg1.textContent=data.Error
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const port=process.env.PORT || 3000
 const path = require("path");
 const hbs = require("hbs");
 const geocode = require("../../weather-app/utils/geocode");
@@ -67,6 +68,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("server running on port 3000");
 });
