3033e6ca09e288ac3eabdd5214456ff0b9a0cf48
Nadeem740 <nadeemblayparambil@gmail.com>
2021-08-22 09:08:52 +0530

    console log call

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,7 @@ const weatherform= document.querySelector('form')
 const search=document.querySelector('input')
 const msg1=document.querySelector('#msg-1')
 const msg2=document.querySelector('#msg-2')
+console.log("hy")
 weatherform.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location=search.value
